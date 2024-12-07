2303abd17b8ac906935da1164625bdd1aa1eda64
0mar-5 <omarmagdy.x1@gmail.com>
2024-12-07 20:12:34 +0200

    adding Gallery component and responsive breakpoints

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Navbar from "./Components/Navbar";
 import Intro from "./Components/Intro";
 import Plans from "./Components/Plans";
+import Gallery from "./Components/Gallery";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className="container">
         <Intro />
         <Plans />
+        <Gallery />
       </div>
     </div>
   );
---
+++ src/Components/Gallery.jsx
@@ -0,0 +1,21 @@
+function Gallery() {
+  const imageURL = [
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122102942/1a-(2).jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103211/13.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103211/15.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103210/12.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122103210/14.jpg",
+    "https://media.geeksforgeeks.org/wp-content/uploads/20240122102942/2b.jpg",
+  ];
+  return (
+    <div className="gallery">
+      {imageURL.map((image, i) => (
+        <div key={i} className="img-container">
+          <img src={image} alt={`image ${i + 1}`} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Gallery;
